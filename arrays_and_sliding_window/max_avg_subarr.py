b0f1e6d0feb493926165475a7acdd6eda71a4d4a
Premkumar Krishnan <prem2822@gmail.com>
2026-06-14 08:58:16 +0530

    Add max_avg_subarr function to calculate the maximum average of a subarray of size k

---
+++ arrays_and_sliding_window/max_avg_subarr.py
@@ -0,0 +1,13 @@
+def max_avg_subarr(nums, k):
+    window_sum = sum(nums[:k])
+    max_sum = window_sum
+
+    for i in range(k, len(nums)):
+        window_sum = window_sum - nums[i - k] + nums[i]
+        max_sum = max(max_sum, window_sum)
+
+    return max_sum / k
+
+nums = [1, 12, -5, -6, 50, 3]
+k = 4
+print(max_avg_subarr(nums, k))
